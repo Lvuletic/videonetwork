8a338d406bba111ca470ad04b505acf9bcdd8c92
lvuletic <luka.vuleti@gmail.com>
2016-08-02 16:39:39 +0200

    basic language file

--- app/messages/en.php
+++ app/messages/en.php
@@ -3,4 +3,16 @@
 $messages = array(
     'videoName' => 'Video name',
     'videoDescription' => 'Video description',
+    'username' => 'Username',
+    'password' => 'Password',
+    'loginError' => 'Username is incorrect', // todo name or pass?
+    'loginGreet' => 'Welcome',
+    'logout' => 'You have been successfully logged out',
+    'logoutLink' => 'Log out',
+    'yourAccount' => 'Your account',
+    'loginLink' => 'Login',
+    'videoNameRequired' => 'Video name is required',
+    'tagNameRequired' => 'Tag name is required',
+    'tagName' => 'Tag name',
+
 );
